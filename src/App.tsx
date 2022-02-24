8a9320ece0a0d826ea69ea5f120e3fe56de7b3d2
LinHeLurking <LinHe.Lurking@gmail.com>
2022-02-24 19:56:48 +0800

    Add I18N

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { useTranslation } from 'react-i18next';
+
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="App">
-      <h2>Informative Minecraft UI</h2>
+      <h1>{t('appTitle')}</h1>
+      <h2>{t('appDescription')}</h2>
     </div>
   )
 }
---
+++ src/i18n/i18n.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import * as zhTranslation from './i18n-zh.json'
+import * as enTranslation from './i18n-en.json'
+
+const resources = {
+    en: enTranslation,
+    zh: zhTranslation
+};
+
+i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        resources,
+        lng: 'zh',
+
+        interpolation: {
+            escapeValue: false // react already safes from xss
+        }
+    });
+
+export default i18n;
