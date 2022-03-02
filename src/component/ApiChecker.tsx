b6c3cb29b00aba4596fc4adf2a3180f82d699189
LinHeLurking <LinHe.Lurking@gmail.com>
2022-03-02 18:40:19 +0800

    align components

--- src/component/ApiChecker.tsx
+++ src/component/ApiChecker.tsx
@@ -5,11 +5,24 @@ import Box from '@mui/material/Box'
 import SvgIcon from '@mui/material/SvgIcon'
 import CircularProgress from '@mui/material/CircularProgress'
 import TextField from '@mui/material/TextField'
+import Stack from '@mui/material/Stack'
 import NotStartedOutlined from '@ricons/material/NotStartedOutlined'
 import ErrorRound from '@ricons/material/ErrorRound'
 import CheckCircleRound from '@ricons/material/CheckCircleRound'
 import ApiList, { Api } from './ApiList'
 
+
+const strLen = (str: string): number => {
+    var len = 0
+    for (var i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) > 127 || str.charCodeAt(i) == 94) {
+            len += 2
+        } else {
+            len++
+        }
+    }
+    return len
+}
 
 
 function ApiChecker() {
@@ -20,14 +33,10 @@ function ApiChecker() {
 
     const concatURL = (tail: string): string => {
         const addr = textInput.length == 0 ? defaultApiURL : textInput
-        if (addr == null) {
-            return tail
+        if (addr.endsWith('/')) {
+            return addr.substring(0, addr.length - 1) + tail
         } else {
-            if (addr.endsWith('/')) {
-                return addr.substring(0, addr.length - 1) + tail
-            } else {
-                return addr + tail
-            }
+            return addr + tail
         }
     }
 
@@ -56,129 +65,146 @@ function ApiChecker() {
                         resolve(true)
                     }
                 }
-                req.open("GET", concatURL(api.url), true)
+                req.open(api.method, concatURL(api.url), true)
                 req.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
                 req.send(null)
-            }, 5000);
+            }, 5000)
         })
     }
 
     const apiChecker: Array<() => void> = []
+    const maxApiNameLen = Math.max(
+        // api names are all english
+        Math.max(...ApiList.map((value) => value.name.length)),
+        // i18n of 'runAllTest' might have chinese
+        strLen(t('runAllTest')),
+    )
 
     return (
-        <Box>
-            <Box sx={{
-                margin: 'auto',
-                marginTop: '20px',
-                width: 'fit-content',
-            }}>
-                <TextField
-                    type='url'
-                    variant="outlined"
-                    label={t('serverApiURL')}
-                    defaultValue={defaultApiURL}
-                    onChange={(event) => {
-                        setTextInput(event.target.value)
-                    }}
-                />
-            </Box>
-            <Box sx={{
-                margin: 'auto',
-                marginTop: '20px',
-                width: 'fit-content',
-                display: 'flex',
-            }}>
-                <IconButton onClick={() => {
-                    apiChecker.forEach((checker) => {
-                        checker()
-                    })
-                }}>
-                    <SvgIcon color='info' fontSize='medium' sx={{
-                        margin: 'auto',
-                    }}>
-                        <NotStartedOutlined />
-                    </SvgIcon>
-                </IconButton>
-                <Box sx={{
-                    margin: 'auto',
-                    width: 'fit-content',
-                }}>
-                    {t('runAllTest')}
+        <Box sx={{
+            margin: 'auto',
+            marginTop: '20px',
+            width: {
+                xs: '60%',
+                sm: '50%',
+                md: '30%',
+                xl: '18%',
+            },
+        }}>
+            <Stack
+                direction='column'
+                spacing={2}
+                alignItems='flex-start'
+                justifyContent='flex-start'
+            >
+                <Box>
+                    <TextField
+                        type='url'
+                        variant='outlined'
+                        label={t('serverApiURL')}
+                        defaultValue={defaultApiURL}
+                        onChange={(event) => {
+                            setTextInput(event.target.value)
+                        }}
+                    />
                 </Box>
-            </Box>
-            {
-                ApiList.map((api) => {
-                    const [firstTime, setFirstTime] = useState(true)
-                    const [testing, setTesting] = useState(false)
-                    const [success, setSuccess] = useState(false)
+                <Stack
+                    direction='row'
+                    spacing={2}
+                    alignItems='center'
+                    justifyContent='flex-start'
+                >
+                    <IconButton onClick={() => {
+                        apiChecker.forEach((checker) => {
+                            checker()
+                        })
+                    }}>
+                        <SvgIcon color='info' fontSize='medium' sx={{
+                            margin: 'auto',
+                        }}>
+                            <NotStartedOutlined />
+                        </SvgIcon>
+                    </IconButton>
+                    <Box sx={{
+                        width: `${maxApiNameLen * 8}px`,
+                    }}>
+                        {t('runAllTest')}
+                    </Box>
+                </Stack>
+                {
+                    ApiList.map((api) => {
+                        const [firstTime, setFirstTime] = useState(true)
+                        const [testing, setTesting] = useState(false)
+                        const [success, setSuccess] = useState(false)
 
-                    const checkThisApi = () => {
-                        if (firstTime) {
-                            setFirstTime(false)
+                        const checkThisApi = () => {
+                            if (firstTime) {
+                                setFirstTime(false)
+                            }
+                            setTesting(true)
+                            console.log(`Testing '${api.name}' by querying '${concatURL(api.url)}'`)
+                            getCheckPromise(api)
+                                .then(value => {
+                                    setTesting(false)
+                                    setSuccess(value)
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                    setTesting(false)
+                                    setSuccess(false)
+                                })
                         }
-                        setTesting(true)
-                        console.log(`Testing '${api.name}' by querying '${concatURL(api.url)}'`)
-                        getCheckPromise(api)
-                            .then(value => {
-                                setTesting(false)
-                                setSuccess(value)
-                            })
-                            .catch(err => {
-                                console.log(err)
-                                setTesting(false)
-                                setSuccess(false)
-                            })
-                    }
 
-                    apiChecker.push(checkThisApi)
+                        apiChecker.push(checkThisApi)
 
-                    return (
-                        <Box
-                            key={api.name}
-                            sx={{
-                                margin: 'auto',
-                                width: 'fit-content',
-                                marginTop: '10px',
-                                display: 'flex',
-                            }}
-                        >
-                            <IconButton onClick={checkThisApi}>
-                                <SvgIcon color='info' fontSize='medium' sx={{
+                        return (
+                            <Stack
+                                key={api.name}
+                                direction='row'
+                                spacing={4}
+                                alignItems='center'
+                                justifyContent='flex-start'
+                            >
+                                <IconButton onClick={checkThisApi}>
+                                    <SvgIcon color='info' fontSize='medium' sx={{
+                                        margin: 'auto',
+                                    }}>
+                                        <NotStartedOutlined />
+                                    </SvgIcon>
+                                </IconButton>
+
+                                <Box sx={{
                                     margin: 'auto',
+                                    width: `${maxApiNameLen * 8}px`,
                                 }}>
-                                    <NotStartedOutlined />
-                                </SvgIcon>
-                            </IconButton>
-                            <Box sx={{
-                                margin: 'auto',
-                                marginLeft: '5px',
-                                width: 'fit-content',
-                            }}>
-                                {api.name}
-                            </Box>
-                            {!firstTime && testing && <CircularProgress color='info' size='16px' sx={{
-                                margin: 'auto',
-                                marginLeft: '5px',
-                            }} />}
-                            {!firstTime && !testing && !success && <IconButton>
-                                <SvgIcon color='error' fontSize='medium' sx={{
-                                    margin: 'auto',
-                                }}>
-                                    <ErrorRound />
-                                </SvgIcon>
-                            </IconButton>}
-                            {!firstTime && !testing && success && <IconButton>
-                                <SvgIcon color='success' fontSize='medium' sx={{
-                                    margin: 'auto',
-                                }}>
-                                    <CheckCircleRound />
-                                </SvgIcon>
-                            </IconButton>}
-                        </Box>
-                    )
-                })
-            }
-        </Box >
+                                    {api.name}
+                                </Box>
+
+                                <Box>
+                                    {!firstTime && testing && <CircularProgress color='info' size='16px' sx={{
+                                        margin: 'auto',
+                                    }} />}
+                                    {!firstTime && !testing && !success && <IconButton>
+                                        <SvgIcon color='error' fontSize='medium' sx={{
+                                            margin: 'auto',
+                                        }}>
+                                            <ErrorRound />
+                                        </SvgIcon>
+                                    </IconButton>}
+                                    {!firstTime && !testing && success && <IconButton>
+                                        <SvgIcon color='success' fontSize='medium' sx={{
+                                            margin: 'auto',
+                                        }}>
+                                            <CheckCircleRound />
+                                        </SvgIcon>
+                                    </IconButton>}
+                                </Box>
+                            </Stack>
+                        )
+                    })
+                }
+            </Stack>
+        </Box>
     )
 }
 
--- src/component/ApiList.ts
+++ src/component/ApiList.ts
@@ -1,6 +1,7 @@
 export interface Api {
     name: string,
     url: string,
+    method: string,
     requiredResponseField: Array<string>,
 }
 
@@ -8,16 +9,19 @@ const ApiList: Array<Api> = [
     {
         name: 'heartbeat',
         url: '/api/system-info/heartbeat',
+        method: 'GET',
         requiredResponseField: ["status"],
     },
     {
         name: 'jvm-info',
         url: '/api/system-info/jvm-info',
+        method: 'GET',
         requiredResponseField: ['javaVersion', 'jvmInfo', 'jvmName', 'jvmVendor', 'jvmVersion', 'kotlinVersion'],
     },
     {
         name: 'os-info',
         url: '/api/system-info/os-info',
+        method: 'GET',
         requiredResponseField: ["allocatedMemory", "freeMemory", "maxMemory", "osName"],
     },
 ]
